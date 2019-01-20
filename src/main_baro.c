9e89f7d0551a78b2b9520ca3c3515a1bd4821a3a
Jonathan Challinger <mr.challinger@gmail.com>
2019-01-20 16:21:33 +0800

    use new driver_ms5611 interface

--- src/main_baro.c
+++ src/main_baro.c
@@ -1,6 +1,5 @@
 #include <hal.h>
 #include <modules/driver_ms5611/driver_ms5611.h>
-#include <modules/worker_thread/worker_thread.h>
 #include <modules/uavcan_debug/uavcan_debug.h>
 #include <uavcan.equipment.air_data.StaticPressure.h>
 #include <uavcan.equipment.air_data.StaticTemperature.h>
@@ -10,54 +9,28 @@
 WORKER_THREAD_DECLARE_EXTERN(WT)
 
 static struct ms5611_instance_s ms5611;
-static struct worker_thread_timer_task_s ms5611_task;
-static void ms5611_task_func(struct worker_thread_timer_task_s* task);
-static struct uavcan_equipment_air_data_StaticTemperature_s temp;
-static struct uavcan_equipment_air_data_StaticPressure_s press;
 
-bool ms5611_initialised;
+static struct worker_thread_listener_task_s ms5611_listener_task;
+static void ms5611_listener_task_func(size_t msg_size, const void* msg, void* ctx);
+
+static struct pubsub_topic_s ms5611_topic;
 
 RUN_AFTER(INIT_END) {
-    for (uint8_t i = 0; i < 5; i++) {
-        if (ms5611_init(&ms5611, 3, BOARD_PAL_LINE_SPI3_MS5611_CS)) {
-            ms5611_initialised = true;
-            ms5611_measure_temperature(&ms5611);
-        }
-        usleep(10000);
-    }
-    worker_thread_add_timer_task(&WT, &ms5611_task, ms5611_task_func, NULL, MS2ST(25), true);
+    pubsub_init_topic(&ms5611_topic, NULL);
+
+    worker_thread_add_listener_task(&WT, &ms5611_listener_task, &ms5611_topic, ms5611_listener_task_func, NULL);
+
+    ms5611_init(&ms5611, 3, BOARD_PAL_LINE_SPI3_MS5611_CS, &WT, &ms5611_topic);
 }
 
-static void ms5611_task_func(struct worker_thread_timer_task_s* task) {
-    (void)task;
-    static uint8_t _state = 0;
-    static uint8_t accum_count;
-    static uint8_t _retry_count = 0;
-    _retry_count++;
-    if (!ms5611_initialised && _retry_count > 50) {
-        if (ms5611_init(&ms5611, 3, BOARD_PAL_LINE_SPI3_MS5611_CS)) {
-            ms5611_initialised = true;
-            ms5611_measure_temperature(&ms5611);
-        }
-        _retry_count = 0;
-    } else if (ms5611_initialised) {
-        if (_state == 0) {
-            ms5611_accum_temperature(&ms5611);
-            ms5611_measure_pressure(&ms5611);
-            _state = 1;
-        } else if (_state == 1) {
-            ms5611_accum_pressure(&ms5611);
-            ms5611_measure_temperature(&ms5611);
-            if (accum_count >= 2) {
-                press.static_pressure = (float)ms5611_read_pressure(&ms5611);
-                temp.static_temperature = ((float)ms5611_read_temperature(&ms5611))/100.0f + 273.15f;
-                uavcan_broadcast(0, &uavcan_equipment_air_data_StaticPressure_descriptor, CANARD_TRANSFER_PRIORITY_HIGH, &press);
-                uavcan_broadcast(0, &uavcan_equipment_air_data_StaticTemperature_descriptor, CANARD_TRANSFER_PRIORITY_HIGH, &temp);
-                accum_count = 0;
-            } else {
-                accum_count++;
-            }
-            _state = 0;
-        }
-    }
+static void ms5611_listener_task_func(size_t msg_size, const void* msg, void* ctx) {
+    struct ms5611_sample_s* sample = (struct ms5611_sample_s*)msg;
+
+    struct uavcan_equipment_air_data_StaticTemperature_s temp;
+    struct uavcan_equipment_air_data_StaticPressure_s press;
+
+    press.static_pressure = sample->pressure_pa;
+    temp.static_temperature = sample->temperature_K;
+    uavcan_broadcast(0, &uavcan_equipment_air_data_StaticPressure_descriptor, CANARD_TRANSFER_PRIORITY_HIGH, &press);
+    uavcan_broadcast(0, &uavcan_equipment_air_data_StaticTemperature_descriptor, CANARD_TRANSFER_PRIORITY_HIGH, &temp);
 }
